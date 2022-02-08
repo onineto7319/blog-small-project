4f39c44808187be849a136d156970cb23540b114
onineto7319 <93433329+onineto7319@users.noreply.github.com>
2022-02-09 00:04:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("test"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
 }
