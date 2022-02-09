32b39e26ffa8ee70420ae6f11ac27db220d075fb
onineto7319@gmail.com <onineto7319@gmail.com>
2022-02-09 22:40:43 +0800

    新增標籤(Tag)及文章(Article)路由

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,6 +3,8 @@ package router
 import (
 	"net/http"
 
+	v1 "github.com/blog-small-project/internal/router/api/v1"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +14,29 @@ func New() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
+	article := v1.GetArticleInstance()
+	tag := v1.GetTagInstance()
+
 	apiv1 := r.Group("/api/v1")
 	{
 		apiv1.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"test": "ok"}) })
+
+	}
+
+	{
+		apiv1.GET("/article/:id", article.GetArticle)
+		apiv1.GET("/articles", article.GetAllArticle)
+		apiv1.POST("/article", article.CreateArticle)
+		apiv1.PUT("/article/:id", article.UpdateArticle)
+		apiv1.DELETE("/article/:id", article.DeleteArticle)
+	}
+
+	{
+		apiv1.GET("/tag/:id", tag.GetTag)
+		apiv1.GET("/tags", tag.GetAllTag)
+		apiv1.POST("/tag", tag.CreateTag)
+		apiv1.PUT("/tag/:id", tag.UpdateTag)
+		apiv1.DELETE("/tag/:id", tag.DeleteTag)
 	}
 
 	return r
